e6c3ddc7947e1489e2da87476cea768fb9cecae6
Bohao Liu <liubohao61@gmail.com>
2020-07-02 14:56:08 -0400

    changes

--- assets/js/departments.js
+++ assets/js/departments.js
@@ -22,17 +22,17 @@
   });
 
   // Mobile?
-  if (browser.mobile) {
-    $body.addClass("is-mobile");
-  } else {
-    breakpoints.on(">medium", function () {
-      $body.removeClass("is-mobile");
-    });
+  // if (browser.mobile) {
+  //   $body.addClass("is-mobile");
+  // } else {
+  //   breakpoints.on(">medium", function () {
+  //     $body.removeClass("is-mobile");
+  //   });
 
-    breakpoints.on("<=medium", function () {
-      $body.addClass("is-mobile");
-    });
-  }
+  //   breakpoints.on("<=medium", function () {
+  //     $body.addClass("is-mobile");
+  //   });
+  // }
 
   // Scrolly.
   $(".scrolly").scrolly({
